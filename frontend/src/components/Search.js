f62494e7daa68c1b11eae6ad25546e42966d2365
Raymond Robinson <rayissach@gmail.com>
2024-10-17 04:04:10 -0400

    Creating functionality to filter fruits queery search in fruits.json

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,32 +1,60 @@
 import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 
 export const Search = () => {
+  const navigate = useNavigate()
   const { username } = useContext(UserContext);
   const [fruits, setFruits] = useState([])
   const [query, setQuery] = useState('')
+  const [selectedFruit, setSelectedFruit] = useState(null)
 
   useEffect(() => {
     const handleFruits = async () => {
-      const response = await fetch('../public/fruits.json')
-      const data = response.json()
-      setFruits(data)
+      try {
+        const response = await fetch('/fruits.json')
+        const data = await response.json()
+        setFruits(data)
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
     }
     handleFruits()
+    
   },[])
 
+    const getFruit = (fruit) => {
+      setSelectedFruit(fruit)
+      setQuery('')
+    }
+
+    const fruitsListed = fruits.filter((fruit) => (
+        fruit.name.toLowerCase().includes(query.toLowerCase())
+      ))
   
 
   return (
     <div>
-      <h1>Hello, {username}</h1>
+      <h1>Hello, {username ? username : 'I am your personal fruit assistant'}</h1>
       <p>Let's Analyze some fruit</p>
       <input 
         placeholder="Type a fruit you'd like to analyze"
         onChange={e => setQuery(e.target.value)}
         value={query}
       />
-      {query}
+      {query && (
+        <div>
+          {/* {fruitsListed.map((fruit) => (
+            <div key={fruit.name} onClick={getFruit(fruit)}>
+              {fruit.name}
+            </div>
+          ))} */}
+        </div>
+      )}
+      <div>
+        <button onClick={() => navigate('/')}>Back</button>
+      </div>
     </div>
   );
 };
