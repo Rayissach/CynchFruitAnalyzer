17fdc6bb5e452d461b92ba7ef4e606602081b321
Raymond Robinson <rayissach@gmail.com>
2024-10-17 01:45:12 -0400

    Using useEffect to render aynchronous data from the api and appending to given array

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,8 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../App';
 
 export const Search = () => {
   const { username } = useContext(UserContext);
+  const [fruits, setFruits] = useState([])
+
+  useEffect(() => {
+    const handleFruits = async () => {
+      const response = await fetch('../public/fruits.json')
+      const data = response.json()
+      setFruits(data)
+    }
+    handleFruits()
+  },[])
 
   return (
     <div>
