0aeb7f9f66b69d255a953c54566ede3deaf175e2
Raymond Robinson <rayissach@gmail.com>
2024-10-16 22:16:57 -0400

    Creating Home Component as well as Search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { WelcomeHome } from './components/WelcomeHome';
 
 function App() {
   return (
     <div className="">
-
+      <WelcomeHome />
     </div>
   );
 }
---
+++ frontend/src/components/WelcomeHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const WelcomeHome = () => {
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
